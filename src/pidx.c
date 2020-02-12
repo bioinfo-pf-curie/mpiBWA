40b6ec2b580d38de194c80b57c94a6599d240bb1
fjarlier <frederic.jarlier@curie.fr>
2020-02-12 09:47:41 +0100

    [FIX] license message

--- src/pidx.c
+++ src/pidx.c
@@ -43,7 +43,7 @@ int main(int argc, char **argv) {
 
 		You can use, modify and/ or redistribute the software under the terms of license (see the LICENSE file for more details).
 
-		The software is distributed in the hope that it will be useful, but "AS IS" WITHOUT ANY WARRANTY OF ANY KIND. Users are therefore encouraged to test the software's suitability as regards their requirements in conditions enabling the security of their systems and/or data. 
+		The software is distributed in the hope that it will be useful, but AS IS WITHOUT ANY WARRANTY OF ANY KIND. Users are therefore encouraged to test the software's suitability as regards their requirements in conditions enabling the security of their systems and/or data. 
 
 		The fact that you are presently reading this means that you have had knowledge of the license and that you accept its terms.
 
